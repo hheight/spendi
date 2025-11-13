a20180417e79e7d182334800dd12651c3fe6f579
Vitalii Shytikov <v.shytikov@gmail.com>
2025-11-13 17:50:20 +0100

    Fix sign up button disappearing

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,11 +1,23 @@
+import Link from "next/link";
+
 export default async function Home() {
   return (
     <div className="flex flex-col items-center justify-center text-center">
       <h1 className="mb-3 text-4xl font-bold">Welcome to Spendi</h1>
-      <p className="text-accent-foreground mb-8 max-w-md">
-        Take control of your finances — track your spending, analyze your habits, and
-        reach your goals.
-      </p>
+      <div className="text-accent-foreground mb-8 max-w-md">
+        <p>Welcome to the Spendi Expense Tracker application.</p>
+        <p>
+          Please{" "}
+          <Link className="underline" href="/login">
+            login
+          </Link>{" "}
+          or{" "}
+          <Link className="underline" href="/signup">
+            create a user
+          </Link>
+          .
+        </p>
+      </div>
     </div>
   );
 }
