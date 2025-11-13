2ee5d4c561e30fa86fcaf5bf3e983ab63cee4db5
Vitalii Shytikov <v.shytikov@gmail.com>
2025-11-13 16:21:54 +0100

    Use migrate dev for tests

--- scripts/db-startup.sh
+++ scripts/db-startup.sh
@@ -7,4 +7,4 @@ docker compose -f compose.yaml -f compose.test.yaml up -d
 echo '🟡 - Waiting for database to be ready...'
 $DIR/wait-for-it.sh localhost:5433 -t 30 -- echo '🟢 - Database is ready!'
 
-npx prisma migrate deploy
+npx prisma migrate dev --name init
