96746b8fafffa28c8e9c26078c759e6f871ac55c
Vitalii Shytikov <v.shytikov@gmail.com>
2025-11-16 13:10:31 +0100

    Show different icons for theme

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -2,7 +2,7 @@
 
 import { useTheme } from "next-themes";
 import { Button } from "@/components/ui/button";
-import { Contrast } from "lucide-react";
+import { Moon, Sun } from "lucide-react";
 
 export default function ThemeToggle() {
   const { theme, setTheme } = useTheme();
@@ -17,7 +17,8 @@ export default function ThemeToggle() {
 
   return (
     <Button title="Toggle theme" variant="ghost" size="icon" onClick={handleClick}>
-      <Contrast />
+      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
     </Button>
   );
 }
