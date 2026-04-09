a34d68b18ef0a84c460ff942f89efd1a74ecc6a9
Vitalii Shytikov <gargles.4arduous@icloud.com>
2026-04-09 20:38:01 +0200

    add refresh token model

---
+++ prisma/migrations/20260409183734_add_refresh_token/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "RefreshToken" (
+    "token" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+    "revokedAt" TIMESTAMP(3),
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "RefreshToken_pkey" PRIMARY KEY ("token")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "RefreshToken_userId_key" ON "RefreshToken"("userId");
+
+-- AddForeignKey
+ALTER TABLE "RefreshToken" ADD CONSTRAINT "RefreshToken_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
